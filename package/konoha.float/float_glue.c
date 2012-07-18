57dc6e385d4f4c41b6b154acdda4183249096de6
Masahiro Ide <imasahiro9@gmail.com>
2012-07-19 04:39:11 +0900

    update konoha.while

--- package/konoha.float/float_glue.c
+++ package/konoha.float/float_glue.c
@@ -22,12 +22,318 @@
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  ***************************************************************************/
 
-#include<minikonoha/minikonoha.h>
-#include<minikonoha/sugar.h>
-#include<minikonoha/float.h>
-#include"float_glue.h"
+#include <minikonoha/minikonoha.h>
+#include <minikonoha/sugar.h>
+#include <minikonoha/float.h>
+#include "ext/mt19937ar.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 // --------------------------------------------------------------------------
+
+// Int
+static void Float_init(KonohaContext *kctx, kObject *o, void *conf)
+{
+	kNumberVar *n = (kNumberVar*)o;  // kFloat has the same structure
+	n->unboxValue = (uintptr_t)conf;  // conf is unboxed data
+}
+
+static void Float_p(KonohaContext *kctx, KonohaStack *sfp, int pos, KUtilsWriteBuffer *wb, int level)
+{
+	KLIB Kwb_printf(kctx, wb, KFLOAT_FMT, sfp[pos].fvalue);
+}
+
+static void kmodfloat_setup(KonohaContext *kctx, struct KonohaModule *def, int newctx)
+{
+}
+
+static void kmodfloat_reftrace(KonohaContext *kctx, struct KonohaModule *baseh)
+{
+}
+
+static void kmodfloat_free(KonohaContext *kctx, struct KonohaModule *baseh)
+{
+	KFREE(baseh, sizeof(kmodfloat_t));
+}
+
+// --------------------------------------------------------------------------
+
+/* float + float */
+static KMETHOD Float_opADD(KonohaContext *kctx, KonohaStack *sfp)
+{
+	RETURNf_(sfp[0].fvalue + sfp[1].fvalue);
+}
+
+static KMETHOD Int_opADD(KonohaContext *kctx, KonohaStack *sfp)
+{
+	RETURNf_(sfp[0].ivalue + sfp[1].fvalue);
+}
+
+/* float - float */
+static KMETHOD Float_opSUB(KonohaContext *kctx, KonohaStack *sfp)
+{
+	RETURNf_(sfp[0].fvalue - sfp[1].fvalue);
+}
+
+static KMETHOD Int_opSUB(KonohaContext *kctx, KonohaStack *sfp)
+{
+	RETURNf_(sfp[0].ivalue - sfp[1].fvalue);
+}
+
+/* float * float */
+static KMETHOD Float_opMUL(KonohaContext *kctx, KonohaStack *sfp)
+{
+	RETURNf_(sfp[0].fvalue * sfp[1].fvalue);
+}
+
+/* float / float */
+static KMETHOD Float_opDIV(KonohaContext *kctx, KonohaStack *sfp)
+{
+	kfloat_t n = sfp[1].fvalue;
+	if(unlikely(n == 0.0)) {
+		kreportf(CritTag, sfp[K_RTNIDX].uline, "Script!!: zero divided");
+	}
+	RETURNf_(sfp[0].fvalue / n);
+}
+
+static KMETHOD Int_opMUL(KonohaContext *kctx, KonohaStack *sfp)
+{
+	RETURNf_(sfp[0].ivalue * sfp[1].fvalue);
+}
+
+/* float / float */
+static KMETHOD Int_opDIV(KonohaContext *kctx, KonohaStack *sfp)
+{
+	kfloat_t n = sfp[1].fvalue;
+	if(unlikely(n == 0.0)) {
+		kreportf(CritTag, sfp[K_RTNIDX].uline, "Script!!: zero divided");
+	}
+	RETURNf_(sfp[0].ivalue / n);
+}
+
+/* float == float */
+static KMETHOD Float_opEQ(KonohaContext *kctx, KonohaStack *sfp)
+{
+	RETURNb_(sfp[0].fvalue == sfp[1].fvalue);
+}
+
+/* float != float */
+static KMETHOD Float_opNEQ(KonohaContext *kctx, KonohaStack *sfp)
+{
+	RETURNb_(sfp[0].fvalue != sfp[1].fvalue);
+}
+
+/* float < float */
+static KMETHOD Float_opLT(KonohaContext *kctx, KonohaStack *sfp)
+{
+	RETURNb_(sfp[0].fvalue < sfp[1].fvalue);
+}
+
+/* float <= float */
+static KMETHOD Float_opLTE(KonohaContext *kctx, KonohaStack *sfp)
+{
+	RETURNb_(sfp[0].fvalue <= sfp[1].fvalue);
+}
+
+/* float > float */
+static KMETHOD Float_opGT(KonohaContext *kctx, KonohaStack *sfp)
+{
+	RETURNb_(sfp[0].fvalue > sfp[1].fvalue);
+}
+
+/* float >= float */
+static KMETHOD Float_opGTE(KonohaContext *kctx, KonohaStack *sfp)
+{
+	RETURNb_(sfp[0].fvalue >= sfp[1].fvalue);
+}
+
+//////
+
+static KMETHOD Int_opEQ(KonohaContext *kctx, KonohaStack *sfp)
+{
+	RETURNb_(sfp[0].ivalue == sfp[1].fvalue);
+}
+
+/* float != float */
+static KMETHOD Int_opNEQ(KonohaContext *kctx, KonohaStack *sfp)
+{
+	RETURNb_(sfp[0].ivalue != sfp[1].fvalue);
+}
+
+
+/* float < float */
+static KMETHOD Int_opLT(KonohaContext *kctx, KonohaStack *sfp)
+{
+	RETURNb_(sfp[0].ivalue < sfp[1].fvalue);
+}
+
+/* float <= float */
+static KMETHOD Int_opLTE(KonohaContext *kctx, KonohaStack *sfp)
+{
+	RETURNb_(sfp[0].ivalue <= sfp[1].fvalue);
+}
+
+/* float > float */
+static KMETHOD Int_opGT(KonohaContext *kctx, KonohaStack *sfp)
+{
+	RETURNb_(sfp[0].ivalue > sfp[1].fvalue);
+}
+
+/* float >= float */
+static KMETHOD Int_opGTE(KonohaContext *kctx, KonohaStack *sfp)
+{
+	RETURNb_(sfp[0].ivalue >= sfp[1].fvalue);
+}
+
+
+/* float to int */
+static KMETHOD Float_toInt(KonohaContext *kctx, KonohaStack *sfp)
+{
+	RETURNi_((kint_t)sfp[0].fvalue);
+}
+
+/* float >= float */
+static KMETHOD Int_toFloat(KonohaContext *kctx, KonohaStack *sfp)
+{
+	RETURNf_((kfloat_t)sfp[0].ivalue);
+}
+
+/* float to String */
+static KMETHOD Float_toString(KonohaContext *kctx, KonohaStack *sfp)
+{
+	char buf[40];
+	PLATAPI snprintf_i(buf, sizeof(buf), KFLOAT_FMT, sfp[0].fvalue);
+	RETURN_(KLIB new_kString(kctx, buf, strlen(buf), SPOL_ASCII));
+}
+
+/* String to float */
+static KMETHOD String_toFloat(KonohaContext *kctx, KonohaStack *sfp)
+{
+	RETURNf_((kfloat_t)strtod(S_text(sfp[0].s), NULL));
+}
+
+//## @Const method Int Int.opMINUS();
+static KMETHOD Float_opMINUS(KonohaContext *kctx, KonohaStack *sfp)
+{
+	RETURNf_(-(sfp[0].fvalue));
+}
+
+//double genrand64_real1(void);
+
+static kfloat_t kfloat_rand(void)
+{
+#if defined(K_USING_NOFLOAT)
+	return (kfloat_t)knh_rand();
+#elif defined(K_USING_INT32)
+	return (kfloat_t)genrand_real1();
+#else
+	return (kfloat_t)genrand64_real1();
+#endif
+}
+
+static KMETHOD Float_random(KonohaContext *kctx, KonohaStack *sfp)
+{
+	RETURNf_(kfloat_rand());
+}
+
+/* ------------------------------------------------------------------------ */
+
+#define _Public   kMethod_Public
+#define _Const    kMethod_Const
+#define _Im       kMethod_Immutable
+#define _Coercion kMethod_Coercion
+#define _Static   kMethod_Static
+#define _F(F)   (intptr_t)(F)
+
+static kbool_t float_initPackage(KonohaContext *kctx, kNameSpace *ns, int argc, const char**args, kfileline_t pline)
+{
+	kmodfloat_t *base = (kmodfloat_t*)KCALLOC(sizeof(kmodfloat_t), 1);
+	base->h.name     = "float";
+	base->h.setup    = kmodfloat_setup;
+	base->h.reftrace = kmodfloat_reftrace;
+	base->h.free     = kmodfloat_free;
+	KLIB Konoha_setModule(kctx, MOD_float, &base->h, pline);
+
+	KDEFINE_CLASS defFloat = {
+		STRUCTNAME(Float),
+		.cflag = CFLAG_Int,
+		.init = Float_init,
+		.p     = Float_p,
+	};
+	base->cFloat = KLIB Konoha_defineClass(kctx, ns->packageId, PN_konoha, NULL, &defFloat, pline);
+	int FN_x = FN_("x");
+	KDEFINE_METHOD MethodData[] = {
+		_Public|_Const|_Im, _F(Float_opADD), TY_Float, TY_Float, MN_("+"), 1, TY_Float, FN_x,
+		_Public|_Const|_Im, _F(Int_opADD), TY_Float, TY_Int, MN_("+"), 1, TY_Float, FN_x,
+		_Public|_Const|_Im, _F(Float_opSUB), TY_Float, TY_Float, MN_("-"), 1, TY_Float, FN_x,
+		_Public|_Const|_Im, _F(Int_opSUB), TY_Float, TY_Int, MN_("+"), 1, TY_Float, FN_x,
+		_Public|_Const|_Im, _F(Float_opMUL), TY_Float, TY_Float, MN_("*"), 1, TY_Float, FN_x,
+		_Public|_Im, _F(Float_opDIV), TY_Float, TY_Float, MN_("/"), 1, TY_Float, FN_x,
+		_Public|_Const|_Im, _F(Float_opEQ),  TY_Boolean, TY_Float, MN_("=="), 1, TY_Float, FN_x,
+		_Public|_Const|_Im, _F(Float_opNEQ), TY_Boolean, TY_Float, MN_("!="), 1, TY_Float, FN_x,
+		_Public|_Const|_Im, _F(Float_opLT),  TY_Boolean, TY_Float, MN_("<"), 1, TY_Float, FN_x,
+		_Public|_Const|_Im, _F(Float_opLTE), TY_Boolean, TY_Float, MN_("<="), 1, TY_Float, FN_x,
+		_Public|_Const|_Im, _F(Float_opGT),  TY_Boolean, TY_Float, MN_(">"), 1, TY_Float, FN_x,
+		_Public|_Const|_Im, _F(Float_opGTE), TY_Boolean, TY_Float, MN_(">="), 1, TY_Float, FN_x,
+		_Public|_Const|_Im, _F(Int_opMUL), TY_Float, TY_Int, MN_("*"), 1, TY_Float, FN_x,
+		_Public|_Im, _F(Int_opDIV), TY_Float, TY_Int, MN_("/"), 1, TY_Float, FN_x,
+		_Public|_Const|_Im, _F(Int_opEQ),  TY_Boolean, TY_Int, MN_("=="), 1, TY_Float, FN_x,
+		_Public|_Const|_Im, _F(Int_opNEQ), TY_Boolean, TY_Int, MN_("!="), 1, TY_Float, FN_x,
+		_Public|_Const|_Im, _F(Int_opLT),  TY_Boolean, TY_Int, MN_("<"), 1, TY_Float, FN_x,
+		_Public|_Const|_Im, _F(Int_opLTE), TY_Boolean, TY_Int, MN_("<="), 1, TY_Float, FN_x,
+		_Public|_Const|_Im, _F(Int_opGT),  TY_Boolean, TY_Int, MN_(">"), 1, TY_Float, FN_x,
+		_Public|_Const|_Im, _F(Int_opGTE), TY_Boolean, TY_Int, MN_(">="), 1, TY_Float, FN_x,
+
+		_Public|_Const|_Im|_Coercion, _F(Float_toInt), TY_Int, TY_Float, MN_to(TY_Int), 0,
+		_Public|_Const|_Im|_Coercion, _F(Int_toFloat), TY_Float, TY_Int, MN_to(TY_Float), 0,
+		_Public|_Const|_Im, _F(Float_toString), TY_String, TY_Float, MN_to(TY_String), 0,
+		_Public|_Const|_Im, _F(String_toFloat), TY_Float, TY_String, MN_to(TY_Float), 0,
+		_Public|_Const|_Im, _F(Float_opMINUS), TY_Float, TY_Float, MN_("-"), 0,
+		_Public|_Static|_Im, _F(Float_random), TY_Float, TY_Float, MN_("random"), 0,
+		DEND,
+	};
+	KLIB kNameSpace_loadMethodData(kctx, ns, MethodData);
+	KDEFINE_FLOAT_CONST FloatData[] = {
+		{"FLOAT_EPSILON", TY_Float, DBL_EPSILON},
+		{}
+	};
+	KLIB kNameSpace_loadConstData(kctx, ns, KonohaConst_(FloatData), pline);
+	return true;
+}
+
+static kbool_t float_setupPackage(KonohaContext *kctx, kNameSpace *ns, kfileline_t pline)
+{
+	return true;
+}
+
+//----------------------------------------------------------------------------
+
+static KMETHOD ExprTyCheck_Float(KonohaContext *kctx, KonohaStack *sfp)
+{
+	VAR_ExprTyCheck(stmt, expr, gma, reqty);
+	kToken *tk = expr->termToken;
+	sfp[4].fvalue = strtod(S_text(tk->text), NULL);   // just using tramsformation float
+	RETURN_(SUGAR kExpr_setUnboxConstValue(kctx, expr, TY_Float, sfp[4].unboxValue));
+}
+
+static kbool_t float_initNameSpace(KonohaContext *kctx,  kNameSpace *ns, kfileline_t pline)
+{
+	KDEFINE_SYNTAX SYNTAX[] = {
+		{ .keyword = SYM_("float"), .type = TY_Float, },
+		{ .keyword = SYM_("double"), .type = TY_Float, },
+		{ .keyword = SYM_("$FLOAT"), ExprTyCheck_(Float), },
+		{ .keyword = KW_END, },
+	};
+	SUGAR kNameSpace_defineSyntax(kctx, ns, SYNTAX);
+	return true;
+}
+
+static kbool_t float_setupNameSpace(KonohaContext *kctx, kNameSpace *ns, kfileline_t pline)
+{
+	return true;
+}
 
 KDEFINE_PACKAGE* float_init(void)
 {
@@ -40,3 +346,7 @@ KDEFINE_PACKAGE* float_init(void)
 	};
 	return &d;
 }
+
+#ifdef __cplusplus
+}
+#endif
