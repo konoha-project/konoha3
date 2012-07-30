e31e2fbd518c46c28f4b0ea2a56884ae86dd4579
kkuramitsu <kimio@konohascript.org>
2012-07-30 23:57:25 +0900

    added konoha.defined package(buggy)

--- package/konoha.defined/defined_glue.c
+++ package/konoha.defined/defined_glue.c
@@ -46,46 +46,32 @@ static kbool_t defined_setupPackage(KonohaContext *kctx, kNameSpace *ns, isFirst
 static KMETHOD ExprTyCheck_Defined(KonohaContext *kctx, KonohaStack *sfp)
 {
 	VAR_ExprTyCheck(stmt, expr, gma, reqty);
-//	kToken *tk = expr->termToken;
-//	sfp[4].definedValue = strtod(S_text(tk->text), NULL);   // just using tramsformation defined
-//	RETURN_(SUGAR kExpr_setUnboxConstValue(kctx, expr, TY_Float, sfp[4].unboxValue));
+	size_t i;
+	kbool_t isDefined = true;
+	for(i = 1; i < kArray_size(expr->cons); i++) {
+		kExpr *typedExpr = SUGAR kStmt_tyCheckByNameAt(kctx, stmt, expr, i, gma, TY_var, 0);
+		if(typedExpr == K_NULLEXPR) {
+			isDefined = false;
+		}
+	}
+	RETURN_(SUGAR kExpr_setUnboxConstValue(kctx, expr, TY_Boolean, isDefined));
 }
 
-//static KMETHOD ExprTyCheck_Defined(KonohaContext *kctx, KonohaStack *sfp)
-//{
-//	VAR_ExprTyCheck(stmt, expr, gma, reqty);
-//	DBG_ASSERT(IS_Expr(kExpr_at(expr, 0)));
-//	DBG_ASSERT(expr->cons->objectItems[1] == K_NULL);
-//	if(Expr_isSymbolTerm(kExpr_at(expr, 0))) {
-//		kMethod *mtd = Expr_lookUpFuncOrMethod(kctx, Stmt_nameSpace(stmt), expr, gma, reqty);
-//		if(mtd != NULL) {
-//			if(Method_isOverloaded(mtd)) {
-//				DBG_P("overloaded found %s.%s%s", Method_t(mtd));
-//				mtd = lookupOverloadedMethod(kctx, stmt, expr, mtd, gma);
-//			}
-//			RETURN_(kStmt_tyCheckCallParamExpr(kctx, stmt, expr, mtd, gma, reqty));
-//		}
-//		if(!TY_isFunc(kExpr_at(expr, 0)->ty)) {
-//			kToken *tk = kExpr_at(expr, 0)->termToken;
-//			DBG_ASSERT(IS_Token(tk));  // TODO: make error message in case of not Token
-//			RETURN_(kToken_p(stmt, tk, ErrTag, "undefined function: %s", Token_text(tk)));
-//		}
-//	}
-//	else {
-//		if(kStmt_tyCheckByNameAt(kctx, stmt, expr, 0, gma, TY_var, 0) != K_NULLEXPR) {
-//			if(!TY_isFunc(expr->cons->exprItems[0]->ty)) {
-//				RETURN_(kExpr_p(stmt, expr, ErrTag, "function is expected"));
-//			}
-//		}
-//	}
-//	RETURN_(Expr_tyCheckFuncParams(kctx, stmt, expr, CT_(kExpr_at(expr, 0)->ty), gma));
-//}
-
+static KMETHOD ParseExpr_Defined(KonohaContext *kctx, KonohaStack *sfp)
+{
+	VAR_ParseExpr(stmt, tokenArray, beginIdx, currentIdx, endIdx);
+	if(beginIdx + 1 == currentIdx) {
+		kTokenVar *definedToken = tokenArray->tokenVarItems[beginIdx];   // defined
+		kTokenVar *pToken = tokenArray->tokenVarItems[beginIdx+1];
+		kExpr *expr = SUGAR new_ConsExpr(kctx, definedToken->resolvedSyntaxInfo, 1, definedToken);
+		RETURN_(SUGAR kStmt_addExprParam(kctx, stmt, expr, pToken->subTokenList, 0, kArray_size(pToken->subTokenList), 0/*isAllowEmpty*/));
+	}
+}
 
 static kbool_t defined_initNameSpace(KonohaContext *kctx,  kNameSpace *ns, kfileline_t pline)
 {
 	KDEFINE_SYNTAX SYNTAX[] = {
-		{ .keyword = SYM_("defined"), .precedence_op1 = 300, ExprTyCheck_(Defined), },
+		{ .keyword = SYM_("defined"), .precedence_op1 = 400, ParseExpr_(Defined), ExprTyCheck_(Defined), },
 		{ .keyword = KW_END, },
 	};
 	SUGAR kNameSpace_defineSyntax(kctx, ns, SYNTAX);
@@ -101,9 +87,9 @@ KDEFINE_PACKAGE* defined_init(void)
 {
 	static KDEFINE_PACKAGE d = {
 		KPACKNAME("defined", "1.0"),
-		.initPackage = defined_initPackage,
-		.setupPackage = defined_setupPackage,
-		.initNameSpace = defined_initNameSpace,
+		.initPackage    = defined_initPackage,
+		.setupPackage   = defined_setupPackage,
+		.initNameSpace  = defined_initNameSpace,
 		.setupNameSpace = defined_setupNameSpace,
 	};
 	return &d;
