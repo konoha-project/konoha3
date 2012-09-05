ba3e01e644ba52c67407a1ec1b3fae98a5d5046b
wakamori <wakamori111@gmail.com>
2012-09-06 06:24:55 +0900

    update posix.process

--- package/posix.process/process_glue.c
+++ package/posix.process/process_glue.c
@@ -22,15 +22,16 @@
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  ***************************************************************************/
 
-#include<minikonoha/minikonoha.h>
-#include<minikonoha/sugar.h>
+#include <minikonoha/minikonoha.h>
+#include <minikonoha/sugar.h>
 #include <unistd.h>
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/resource.h>
 #include <sys/time.h>
 #include <grp.h>
-//## @Static @Public Int System.getpid();
+
+/* ------------------------------------------------------------------------ */
 
 static KMETHOD System_getpid(KonohaContext *kctx, KonohaStack *sfp)
 {
@@ -133,6 +134,98 @@ static KMETHOD System_setgroups(KonohaContext *kctx, KonohaStack *sfp)
 	RETURNi_(ret);
 }
 
+//## Int System.fork()
+static KMETHOD System_fork(KonohaContext *kctx, KonohaStack *sfp)
+{
+	pid_t pid = fork();
+	RETURNi_(pid);
+}
+
+//## Int System.wait()
+static KMETHOD System_wait(KonohaContext *kctx, KonohaStack *sfp)
+{
+	int status = 0;
+	pid_t pid = wait(&status);
+	RETURNi_(pid);
+}
+
+//## Int System.waitpid(Int pid, Int options)
+static KMETHOD System_waitpid(KonohaContext *kctx, KonohaStack *sfp)
+{
+	int status = 0;
+	pid_t target_pid = sfp[1].intValue;
+	int options = sfp[2].intValue;
+	pid_t pid = waitpid(target_pid, &status, options);
+	RETURNi_(pid);
+}
+
+//## Int System.setuid(Int uid)
+static KMETHOD System_setuid(KonohaContext *kctx, KonohaStack *sfp)
+{
+	uid_t uid = sfp[1].intValue;
+	int ret = setuid(uid);
+	RETURNi_(ret);
+}
+
+//## Int System.seteuid(Int euid)
+static KMETHOD System_seteuid(KonohaContext *kctx, KonohaStack *sfp)
+{
+	uid_t euid = sfp[1].intValue;
+	int ret = seteuid(euid);
+	RETURNi_(ret);
+}
+
+//## Int System.setreuid(Int ruid, Int euid)
+static KMETHOD System_setreuid(KonohaContext *kctx, KonohaStack *sfp)
+{
+	uid_t ruid = sfp[1].intValue;
+	uid_t euid = sfp[2].intValue;
+	int ret = setreuid(ruid, euid);
+	RETURNi_(ret);
+}
+
+//## Int System.setgid(Int gid)
+static KMETHOD System_setgid(KonohaContext *kctx, KonohaStack *sfp)
+{
+	uid_t gid = sfp[1].intValue;
+	int ret = setgid(gid);
+	RETURNi_(ret);
+}
+
+//## Int System.setegid(Int euid)
+static KMETHOD System_setegid(KonohaContext *kctx, KonohaStack *sfp)
+{
+	uid_t egid = sfp[1].intValue;
+	int ret = setegid(egid);
+	RETURNi_(ret);
+}
+
+//## Int System.setregid(Int rgid, Int egid)
+static KMETHOD System_setregid(KonohaContext *kctx, KonohaStack *sfp)
+{
+	uid_t rgid = sfp[1].intValue;
+	uid_t egid = sfp[2].intValue;
+	int ret = setregid(rgid, egid);
+	RETURNi_(ret);
+}
+
+//## Int System.setsid()
+static KMETHOD System_setsid(KonohaContext *kctx, KonohaStack *sfp)
+{
+	pid_t ret = setsid();
+	RETURNi_(ret);
+}
+
+//## Int System.getsid(Int pid)
+static KMETHOD System_getsid(KonohaContext *kctx, KonohaStack *sfp)
+{
+	pid_t pid = sfp[1].intValue;
+	pid_t ret = getsid(pid);
+	RETURNi_(ret);
+}
+
+/* ------------------------------------------------------------------------ */
+
 #define _Public   kMethod_Public
 #define _Const    kMethod_Const
 #define _Static   kMethod_Static
@@ -164,6 +257,17 @@ static	kbool_t process_initPackage(KonohaContext *kctx, kNameSpace *ns, int argc
 		_Public|_Static, _F(System_setpriority), TY_int, TY_System, MN_("setpriority"), 3, TY_int, FN_("which"), TY_int, FN_("who"), TY_int, FN_("priority"),
 		_Public|_Static, _F(System_getgroups), TY_int, TY_System, MN_("getgroups"), 2, TY_int, FN_("size"), TY_intArray, FN_("list[]"),
 		_Public|_Static, _F(System_setgroups), TY_int, TY_System, MN_("setgroups"), 2, TY_int, FN_("size"), TY_intArray, FN_("*list"),
+		_Public|_Static, _F(System_fork), TY_int, TY_System, MN_("fork"), 0,
+		_Public|_Static, _F(System_wait), TY_int, TY_System, MN_("wait"), 0,
+		_Public|_Static, _F(System_waitpid), TY_int, TY_System, MN_("wait"), 2, TY_int, FN_("pid"), TY_int, FN_("options"),
+		_Public|_Static, _F(System_setuid), TY_int, TY_System, MN_("setuid"), 1, TY_int, FN_("uid"),
+		_Public|_Static, _F(System_seteuid), TY_int, TY_System, MN_("seteuid"), 1, TY_int, FN_("euid"),
+		_Public|_Static, _F(System_setreuid), TY_int, TY_System, MN_("setreuid"), 2, TY_int, FN_("ruid"), TY_int, FN_("euid"),
+		_Public|_Static, _F(System_setgid), TY_int, TY_System, MN_("setgid"), 1, TY_int, FN_("gid"),
+		_Public|_Static, _F(System_setegid), TY_int, TY_System, MN_("setguid"), 1, TY_int, FN_("egid"),
+		_Public|_Static, _F(System_setregid), TY_int, TY_System, MN_("setrguid"), 2, TY_int, FN_("rgid"), TY_int, FN_("egid"),
+		_Public|_Static, _F(System_setsid), TY_int, TY_System, MN_("setsid"), 0,
+		_Public|_Static, _F(System_getsid), TY_int, TY_System, MN_("getsid"), 1, TY_int, FN_("pid"),
 		DEND,
 	};
 	KLIB kNameSpace_loadMethodData(kctx, ns, MethodData);
@@ -196,7 +300,7 @@ static kbool_t process_setupNameSpace(KonohaContext *kctx, kNameSpace *ns, kfile
 KDEFINE_PACKAGE* process_init(void)
 {
 	static KDEFINE_PACKAGE d = {
-		KPACKNAME("package.posix", "1.0"),
+		KPACKNAME("posix.process", "1.0"),
 		KPACKLIB("POSIX.1", "1.0"),
 		.initPackage = process_initPackage,
 		.setupPackage = process_setupPackage,
