042ca895ac8b7391ef9ad713059f489fdcbebc64
Masahiro Ide <imasahiro9@gmail.com>
2012-09-27 08:50:36 +0900

    fix build error at sugar.h

--- include/minikonoha/sugar.h
+++ include/minikonoha/sugar.h
@@ -345,7 +345,7 @@ typedef enum {
 	TSTMT_JUMP,
 	TSTMT_TRY,
 
-	TSTMT_ERR,   // this must be last
+	TSTMT_ERR   // this must be last
 } kvisit_t;
 
 #define TEXPR_UNTYPED       -1   /*THIS MUST NOT HAPPEN*/
