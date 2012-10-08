2c966daecccd6edb78b7e4a5335e5833524f6148
kkuramitsu <kimio@konohascript.org>
2012-10-08 18:17:03 +0900

    adding cstyle package

---
+++ package/cstyle/cstyle_glue.c
@@ -0,0 +1,101 @@
+/****************************************************************************
+ * Copyright (c) 2012, the Konoha project authors. All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ***************************************************************************/
+
+/* ************************************************************************ */
+
+#include <minikonoha/minikonoha.h>
+#include <minikonoha/sugar.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static KMETHOD Statement_while(KonohaContext *kctx, KonohaStack *sfp)
+{
+	VAR_Statement(stmt, gma);
+	DBG_P("while statement .. ");
+	int ret = false;
+	if(SUGAR kStmt_tyCheckByName(kctx, stmt, KW_ExprPattern, gma, TY_boolean, 0)) {
+		kBlock *bk = SUGAR kStmt_getBlock(kctx, stmt, NULL/*DefaultNameSpace*/, KW_BlockPattern, K_NULLBLOCK);
+		Stmt_setCatchContinue(stmt, true);  // set before tyCheckAll
+		Stmt_setCatchBreak(stmt, true);
+		ret = SUGAR kBlock_tyCheckAll(kctx, bk, gma);
+		if(ret) {
+			kStmt_typed(stmt, LOOP);
+		}
+	}
+	RETURNb_(ret);
+}
+
+// --------------------------------------------------------------------------
+
+static kbool_t cstyle_initPackage(KonohaContext *kctx, kNameSpace *ns, int argc, const char**args, kfileline_t pline)
+{
+	KImportPackage(ns, "konoha.break", pline);
+	KImportPackage(ns, "konoha.continue", pline);
+	KDEFINE_SYNTAX SYNTAX[] = {
+		{ SYM_("while"), 0, "\"while\" \"(\" $Expr \")\" $Block", 0, 0, NULL, NULL, NULL, Statement_while, NULL, },
+		{ KW_END, },
+	};
+	SUGAR kNameSpace_defineSyntax(kctx, ns, SYNTAX, ns);
+	return true;
+}
+
+static kbool_t cstyle_setupPackage(KonohaContext *kctx, kNameSpace *ns, isFirstTime_t isFirstTime, kfileline_t pline)
+{
+
+	return true;
+}
+
+// --------------------------------------------------------------------------
+
+static inline kStmt* kStmt_getParentNULL(kStmt *stmt)
+{
+	return stmt->parentBlockNULL->parentStmtNULL;
+}
+
+static kbool_t cstyle_initNameSpace(KonohaContext *kctx, kNameSpace *packageNameSpace, kNameSpace *ns, kfileline_t pline)
+{
+	return true;
+}
+
+static kbool_t cstyle_setupNameSpace(KonohaContext *kctx, kNameSpace *packageNameSpace, kNameSpace *ns, kfileline_t pline)
+{
+	return true;
+}
+
+KDEFINE_PACKAGE* cstyle_init(void)
+{
+	static KDEFINE_PACKAGE d = {0};
+	KSETPACKNAME(d, "cstyle", "1.0");
+	d.initPackage    = cstyle_initPackage;
+	d.setupPackage   = cstyle_setupPackage;
+	d.initNameSpace  = cstyle_initNameSpace;
+	d.setupNameSpace = cstyle_setupNameSpace;
+	return &d;
+}
+
+#ifdef __cplusplus
+}
+#endif
