cbb29cf0fe55d327e0f850154f32a30c4f6b0cdb
kkuramitsu <kimio@konohascript.org>
2012-10-17 22:53:54 +0900

    bugs fixed at konoha.global

--- package/konoha.global/global_glue.c
+++ package/konoha.global/global_glue.c
@@ -131,7 +131,7 @@ static kbool_t global_defineSyntax(KonohaContext *kctx, kNameSpace *ns, KTraceIn
 {
 	KImportPackage(ns, "konoha.field", trace);
 	SUGAR kNameSpace_addSugarFunc(kctx, ns, KW_TypeDeclPattern, SugarFunc_TopLevelStatement, new_SugarFunc(ns, Statement_GlobalTypeDecl));
-	return kNameSpace_initGlobalObject(kctx, ns, trace);
+	return true;
 }
 
 // -------
