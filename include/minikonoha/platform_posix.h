ce41e1c39f58d510c186d955a71435de2b7e6ee1
masakiishii <masaki.ishii511@gmail.com>
2012-10-22 21:14:44 +0900

    modified

--- include/minikonoha/platform_posix.h
+++ include/minikonoha/platform_posix.h
@@ -811,7 +811,10 @@ static kbool_t fetch_CoverageLog_from_Berkeley_DB(KonohaContext *kctx, const cha
 			goto err;
 		}
 		if(strncmp(key, (const char *)DB_key.data, strlen(key)) == 0) {
-			printf("%.*s\n", (int)DB_data.size, (char *)DB_data.data);
+			fprintf(stderr, "key %s\n", key);
+			fprintf(stderr, "key.data %s\n", (const char *) DB_key.data);
+			fprintf(stderr, "coverage cleared %.*s%.*s\n", (int)DB_data.size, (char *)DB_data.data);
+			//printf("%.*s\n", (int)DB_data.size, (char *)DB_data.data);
 			dbcp->c_close(dbcp);
 			dbp->close(dbp, 0);
 			return true;
@@ -843,7 +846,7 @@ static kbool_t DEOS_checkSoftwareTestIsPass(KonohaContext *kctx, const char *fil
 #define N 64
 	kbool_t res;
 	char key[N] = {'\0'};
-	snprintf(key, N, "\"%s/%d\"", filename, line);
+	snprintf(key, N, "\"%s:%d\"", filename, line);
 	res = fetch_CoverageLog_from_Berkeley_DB(kctx, key);
 	return res;
 #else
