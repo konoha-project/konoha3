a90efbd2d39eb9f384f6f3fa83f568cfb58cde53
Masahiro Ide <imasahiro9@gmail.com>
2012-11-06 04:39:21 +0900

    fix build error at llvm package

--- include/minikonoha/minikonoha.h
+++ include/minikonoha/minikonoha.h
@@ -1521,7 +1521,7 @@ typedef enum {
 	CrossCompile  = 1,
 	HatedLazyCompile   = (1 < 1),
 	O2Compile     = (1 < 2),
-	DebugCompile  = (1 < 3),
+	DebugCompile  = (1 < 3)
 } CompileOption;
 
 typedef struct KObjectVisitor {
