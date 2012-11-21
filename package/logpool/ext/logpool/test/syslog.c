2a9144aea1d5b0b1ab81fa2b39a6fdf1a5d840aa
Masahiro Ide <imasahiro9@gmail.com>
2012-11-21 16:15:19 +0900

    remove logpool and jit package

--- package/logpool/ext/logpool/test/syslog.c
+++
@@ -1,7 +0,0 @@
-/* syslog api test */
-#include "logpool.h"
-static struct logpool_param_syslog SYSLOG_API_PARAM = {8, 1024};
-#define LOGAPI_PARAM cast(logpool_param_t *, &SYSLOG_API_PARAM)
-#define LOGAPI_INIT_FLAG (LOGPOOL_DEFAULT)
-#define LOGAPI SYSLOG_API
-#include "test_main.c"
--- package/logpool/ext/logpool/tools/logpoold.c
+++
@@ -1,6 +0,0 @@
-#include "logpool.h"
-
-int main(int argc, char **argv)
-{
-    return logpoold_start("0.0.0.0", 14801);
-}
