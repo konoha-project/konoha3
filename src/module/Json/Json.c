konoha-project/konoha3

commit 72d1f909442fe73e22e24763f7b1da4c215c25e9

update Json module

src/module/Json/Json.c
@@ -35,21 +35,22 @@ extern "C" {
 struct JsonBuf {
 	uint64_t json_i;
 };
+#define AsJSON(JSONBUF) (toJSON(jsonbuf->json_i))
 
 static kbool_t IsJsonType(struct JsonBuf *jsonbuf, KJSONTYPE type)
 {
 	switch(type) {
-	case KJSON_OBJECT:  return JSON_TYPE_CHECK(Object, toJSON(jsonbuf->json_i));
-	case KJSON_ARRAY:   return JSON_TYPE_CHECK(Array , toJSON(jsonbuf->json_i));
-	case KJSON_STRING:  return JSON_TYPE_CHECK(String, toJSON(jsonbuf->json_i));
-	case KJSON_INT:     return JSON_TYPE_CHECK(Int32 , toJSON(jsonbuf->json_i));
-	case KJSON_DOUBLE:  return JSON_TYPE_CHECK(Double, toJSON(jsonbuf->json_i));
-	case KJSON_BOOLEAN: return JSON_TYPE_CHECK(Bool  , toJSON(jsonbuf->json_i));
-	case KJSON_NULL:    return JSON_TYPE_CHECK(Null  , toJSON(jsonbuf->json_i));
-	case KJSON_INT64:   return JSON_TYPE_CHECK(Int64 , toJSON(jsonbuf->json_i));
-	case KJSON_LONG:    return JSON_TYPE_CHECK(Int32 , toJSON(jsonbuf->json_i));
+	case KJSON_OBJECT:  return JSON_TYPE_CHECK(Object, AsJSON(jsonbuf));
+	case KJSON_ARRAY:   return JSON_TYPE_CHECK(Array , AsJSON(jsonbuf));
+	case KJSON_STRING:  return JSON_TYPE_CHECK(String, AsJSON(jsonbuf));
+	case KJSON_INT:     return JSON_TYPE_CHECK(Int32 , AsJSON(jsonbuf));
+	case KJSON_DOUBLE:  return JSON_TYPE_CHECK(Double, AsJSON(jsonbuf));
+	case KJSON_BOOLEAN: return JSON_TYPE_CHECK(Bool  , AsJSON(jsonbuf));
+	case KJSON_NULL:    return JSON_TYPE_CHECK(Null  , AsJSON(jsonbuf));
+	case KJSON_INT64:   return JSON_TYPE_CHECK(Int64 , AsJSON(jsonbuf));
+	case KJSON_LONG:    return JSON_TYPE_CHECK(Int32 , AsJSON(jsonbuf));
 	}
-	return false; /* imf */
+	return false;
 }
 
 static uint64_t NewJsonI(JSONMemoryPool *pool, KJSONTYPE type, va_list ap)
@@ -82,36 +83,38 @@ static struct JsonBuf *CreateJson(KonohaContext *kctx, struct JsonBuf *jsonbuf, 
 
 static kbool_t ParseJson(KonohaContext *kctx, struct JsonBuf *jsonbuf, const char *text, size_t length, KTraceInfo *trace)
 {
-	jsonbuf->json_i = parseJSON((JSONMemoryPool *)(PLATAPI JsonHandler), text, text + length).bits;
+	JSON json = parseJSON((JSONMemoryPool *)(PLATAPI JsonHandler), text, text + length);
+	if (IsError(json.val)) {
+		KLIB KRuntime_raise(kctx, KException_("InvalidJsonText"), SoftwareFault, NULL, trace->baseStack);
+	}
+	jsonbuf->json_i = json.bits;
 	return jsonbuf->json_i != 0;
 }
 
 static void FreeJson(KonohaContext *kctx, struct JsonBuf *jsonbuf)
 {
-	JSON_free(toJSON(jsonbuf->json_i));
+	JSON_free(AsJSON(jsonbuf));
 }
 
 static const char *JsonToNewText(KonohaContext *kctx, struct JsonBuf *jsonbuf)
 {
 	size_t length;
-	const char *text = JSON_toStringWithLength(toJSON(jsonbuf->json_i), &length);
+	const char *text = JSON_toStringWithLength(AsJSON(jsonbuf), &length);
 	return text; /* need free */
 }
 
 static size_t DoJsonEach(KonohaContext *kctx, struct JsonBuf *jsonbuf, void *thunk, void (*doEach)(KonohaContext *, const char *, struct JsonBuf *, void *))
 {
 	size_t count = 0;
-	struct JsonBuf eachbuf = {};
-	{
-		JSON Key, Val;
-		JSONObject_iterator Itr;
-		JSON obj = toJSON(jsonbuf->json_i);
-		JSON_OBJECT_EACH(obj, Itr, Key, Val) {
-			const char *key = Key.str->str;
-			eachbuf.json_i = Val.bits;
-			doEach(kctx, key, &eachbuf, thunk);
-			count++;
-		}
+	struct JsonBuf eachbuf;
+	JSON Key, Val;
+	JSONObject_iterator Itr;
+	JSON obj = AsJSON(jsonbuf);
+	JSON_OBJECT_EACH(obj, Itr, Key, Val) {
+		const char *key = Key.str->str;
+		eachbuf.json_i = Val.bits;
+		doEach(kctx, key, &eachbuf, thunk);
+		count++;
 	}
 	return count;
 }
@@ -120,18 +123,20 @@ static size_t DoJsonEach(KonohaContext *kctx, struct JsonBuf *jsonbuf, void *thu
 
 static kbool_t RetrieveJsonKeyValue(KonohaContext *kctx, struct JsonBuf *jsonbuf, const char *key, size_t keylen_or_zero, struct JsonBuf *newbuf)
 {
-	newbuf->json_i = JSON_get(toJSON(jsonbuf->json_i), key, KeyLen(key, keylen_or_zero)).bits;
+	JSON json = JSON_get(AsJSON(jsonbuf), key, KeyLen(key, keylen_or_zero));
+	JSONObject_Retain(json);
+	newbuf->json_i = json.bits;
 	return newbuf->json_i != 0;
 }
 
 static kbool_t SetJsonKeyValue(KonohaContext *kctx, struct JsonBuf *jsonbuf, const char *key, size_t keylen_or_zero, struct JsonBuf *otherbuf)
 {
-	if(!JSON_TYPE_CHECK(Object, toJSON(jsonbuf->json_i))) {
+	JSON json = AsJSON(jsonbuf);
+	if(!JSON_TYPE_CHECK(Object, json)) {
 		return false;
 	}
 	size_t keylen = KeyLen(key, keylen_or_zero);
-	JSONObject_set((JSONMemoryPool *)(PLATAPI JsonHandler), toJSON(jsonbuf->json_i),
-			key, keylen, toJSON(otherbuf->json_i));
+	JSONObject_set((JSONMemoryPool *)(PLATAPI JsonHandler), json, key, keylen, AsJSON(otherbuf));
 	return true;
 }
 
@@ -143,8 +148,7 @@ static kbool_t SetJsonValue(KonohaContext *kctx, struct JsonBuf *jsonbuf, const 
 	kbool_t ret = true;
 	if(key != NULL) {
 		size_t keylen = KeyLen(key, keylen_or_zero);
-		JSONObject_set((JSONMemoryPool *)(PLATAPI JsonHandler),
-				toJSON(jsonbuf->json_i), key, keylen, val);
+		JSONObject_set((JSONMemoryPool *)(PLATAPI JsonHandler), AsJSON(jsonbuf), key, keylen, val);
 	}
 	else {
 		jsonbuf->json_i = val.bits;
@@ -155,45 +159,52 @@ static kbool_t SetJsonValue(KonohaContext *kctx, struct JsonBuf *jsonbuf, const 
 
 static kbool_t GetJsonBoolean(KonohaContext *kctx, struct JsonBuf *jsonbuf, const char *key, size_t keylen_or_zero, kbool_t defval)
 {
+	JSON json = AsJSON(jsonbuf);
 	if(key == NULL)
-		return JSONBool_get(toJSON(jsonbuf->json_i));
-	return JSON_getBool(toJSON(jsonbuf->json_i), key, KeyLen(key, keylen_or_zero));
+		return JSONBool_get(json);
+	return JSON_getBool(json, key, KeyLen(key, keylen_or_zero));
 }
 
 static int64_t GetJsonInt(KonohaContext *kctx, struct JsonBuf *jsonbuf, const char *key, size_t keylen_or_zero, int64_t defval)
 {
+	JSON json = AsJSON(jsonbuf);
 	if(key == NULL) {
-		if(JSON_TYPE_CHECK(Int32, toJSON(jsonbuf->json_i))) {
-			return JSONInt_get(toJSON(jsonbuf->json_i));
+		if(JSON_TYPE_CHECK(Int32, json)) {
+			return JSONInt_get(json);
 		}
 	}
-	return JSON_getInt(toJSON(jsonbuf->json_i), key, KeyLen(key, keylen_or_zero));
+	return JSON_getInt(json, key, KeyLen(key, keylen_or_zero));
 }
 
 static double GetJsonFloat(KonohaContext *kctx, struct JsonBuf *jsonbuf, const char *key, size_t keylen_or_zero, double defval)
 {
+	JSON json = AsJSON(jsonbuf);
 	if(key == NULL)
-		JSONDouble_get(toJSON(jsonbuf->json_i));
-	return JSON_getDouble(toJSON(jsonbuf->json_i), key, KeyLen(key, keylen_or_zero));
+		JSONDouble_get(json);
+	return JSON_getDouble(json, key, KeyLen(key, keylen_or_zero));
 }
 
 static const char *GetJsonText(KonohaContext *kctx, struct JsonBuf *jsonbuf, const char *key, size_t keylen_or_zero, const char *defval)
 {
+	JSON json = AsJSON(jsonbuf);
 	if(key == NULL)
-		JSONString_get(toJSON(jsonbuf->json_i));
+		JSONString_get(json);
 	size_t length = KeyLen(key, keylen_or_zero);
-	return JSON_getString(toJSON(jsonbuf->json_i), key, &length);
+	return JSON_getString(json, key, &length);
 }
 
 static size_t GetJsonSize(KonohaContext *kctx, struct JsonBuf *jsonbuf)
 {
-	return JSON_length(toJSON(jsonbuf->json_i));
+	return JSON_length(AsJSON(jsonbuf));
 }
 
 static kbool_t RetrieveJsonArrayAt(KonohaContext *kctx, struct JsonBuf *jsonbuf, size_t index, struct JsonBuf *otherbuf)
 {
-	if(JSON_TYPE_CHECK(Array, toJSON(jsonbuf->json_i))) {
-		otherbuf->json_i = JSONArray_get(toJSON(jsonbuf->json_i), index).bits;
+	JSON json = AsJSON(jsonbuf);
+	if(JSON_TYPE_CHECK(Array, json)) {
+		JSON ret = JSONArray_get(json, index);
+		JSONObject_Retain(ret);
+		otherbuf->json_i = ret.bits;
 		return true;
 	}
 	return false;
@@ -201,8 +212,9 @@ static kbool_t RetrieveJsonArrayAt(KonohaContext *kctx, struct JsonBuf *jsonbuf,
 
 static kbool_t SetJsonArrayAt(KonohaContext *kctx, struct JsonBuf *jsonbuf, size_t index, struct JsonBuf *otherbuf)
 {
-	if(JSON_TYPE_CHECK(Array, toJSON(jsonbuf->json_i))) {
-		JSONArray_set(toJSON(jsonbuf->json_i), index, toJSON(otherbuf->json_i));
+	JSON json = AsJSON(jsonbuf);
+	if(JSON_TYPE_CHECK(Array, json)) {
+		JSONArray_set(json, index, toJSON(otherbuf->json_i));
 		return true;
 	}
 	return false;
@@ -210,9 +222,9 @@ static kbool_t SetJsonArrayAt(KonohaContext *kctx, struct JsonBuf *jsonbuf, size
 
 static kbool_t AppendJsonArray(KonohaContext *kctx, struct JsonBuf *jsonbuf, struct JsonBuf *otherbuf)
 {
-	if(JSON_TYPE_CHECK(Array, toJSON(jsonbuf->json_i))) {
-		JSONArray_append((JSONMemoryPool *)(PLATAPI JsonHandler),
-				toJSON(jsonbuf->json_i), toJSON(otherbuf->json_i));
+	JSON json = AsJSON(jsonbuf);
+	if(JSON_TYPE_CHECK(Array, json)) {
+		JSONArray_append((JSONMemoryPool *)(PLATAPI JsonHandler), json, toJSON(otherbuf->json_i));
 		return true;
 	}
 	return false;
@@ -225,26 +237,25 @@ kbool_t LoadJsonModule(KonohaFactory *factory, ModuleType type)
 	static KModuleInfo ModuleInfo = {
 		"Json", "0.1", 0, "json",
 	};
-	factory->JsonDataInfo            = &ModuleInfo;
-	factory->IsJsonType             = IsJsonType;
-	factory->CreateJson             = CreateJson;
-	factory->ParseJson              = ParseJson;
-	factory->FreeJson               = FreeJson;
-	factory->JsonToNewText          = JsonToNewText;
-	factory->DoJsonEach             = DoJsonEach;
-	factory->RetrieveJsonKeyValue   = RetrieveJsonKeyValue;
-	factory->SetJsonKeyValue        = SetJsonKeyValue;
-	factory->SetJsonValue           = SetJsonValue;
-	factory->GetJsonBoolean         = GetJsonBoolean;
-	factory->GetJsonInt             = GetJsonInt;
-	factory->GetJsonFloat           = GetJsonFloat;
-	factory->GetJsonText            = GetJsonText;
-	factory->GetJsonSize            = GetJsonSize;
-	factory->RetrieveJsonArrayAt = RetrieveJsonArrayAt;
-	factory->SetJsonArrayAt      = SetJsonArrayAt;
-	factory->AppendJsonArray        = AppendJsonArray;
-
-	factory->JsonHandler            = (JSONMemoryPool *) malloc(sizeof(JSONMemoryPool));
+	factory->JsonDataInfo         = &ModuleInfo;
+	factory->IsJsonType           = IsJsonType;
+	factory->CreateJson           = CreateJson;
+	factory->ParseJson            = ParseJson;
+	factory->FreeJson             = FreeJson;
+	factory->JsonToNewText        = JsonToNewText;
+	factory->DoJsonEach           = DoJsonEach;
+	factory->RetrieveJsonKeyValue = RetrieveJsonKeyValue;
+	factory->SetJsonKeyValue      = SetJsonKeyValue;
+	factory->SetJsonValue         = SetJsonValue;
+	factory->GetJsonBoolean       = GetJsonBoolean;
+	factory->GetJsonInt           = GetJsonInt;
+	factory->GetJsonFloat         = GetJsonFloat;
+	factory->GetJsonText          = GetJsonText;
+	factory->GetJsonSize          = GetJsonSize;
+	factory->RetrieveJsonArrayAt  = RetrieveJsonArrayAt;
+	factory->SetJsonArrayAt       = SetJsonArrayAt;
+	factory->AppendJsonArray      = AppendJsonArray;
+	factory->JsonHandler          = (JSONMemoryPool *) malloc(sizeof(JSONMemoryPool));
 	JSONMemoryPool_Init((JSONMemoryPool *)factory->JsonHandler);
 	return true;
 }

src/module/Json/kjson/.gitignore
@@ -1,9 +1,0 @@
-Makefile
-build
-Debug
-Release
-.DS_Store
-GPATH
-GRTAGS
-GTAGS
-a.out*

src/module/Json/kjson/.travis.yml
@@ -1,15 +1,0 @@
-language: c
-compiler:
-  - clang
-  - gcc
-
-before_install:
-  - "sudo apt-get install cmake"
-
-before_script:
-  - "mkdir -p build"
-  - "cd build"
-  - "cmake ../"
-  - "make"
-
-script: "make test"

src/module/Json/kjson/CMakeLists.txt
@@ -1,153 +1,0 @@
-include(CheckTypeSize)
-include(CheckIncludeFiles)
-
-cmake_minimum_required(VERSION 2.4)
-project(kjson)
-
-include_directories(${CMAKE_CURRENT_SOURCE_DIR})
-
-if(NOT CMAKE_BUILD_TYPE)
-    set(CMAKE_BUILD_TYPE "Release")
-endif()
-
-SET(CFLAGS "-Wsign-compare -Wmissing-field-initializers -Wdisabled-optimization")
-SET(CFLAGS "${CFLAGS} -Wall -Wcomment -Wformat -Wunused-macros")
-SET(CMAKE_C_FLAGS         "-Wall -Wno-variadic-macros ${CFLAGS}")
-SET(CMAKE_C_FLAGS_RELEASE "-O3 -g")
-SET(CMAKE_C_FLAGS_DEBUG   "-O0 -g3")
-string(TOUPPER ${CMAKE_BUILD_TYPE} uppercase_CMAKE_BUILD_TYPE)
-
-if(${CMAKE_BUILD_TYPE} STREQUAL "Release")
-    add_definitions(-DNDEBUG)
-endif()
-
-set(JSON_SRC kjson.c)
-add_library(kjson SHARED ${JSON_SRC})
-
-## Test cases
-file(GLOB JSON_TEST ${CMAKE_CURRENT_SOURCE_DIR}/test/test*.json)
-file(GLOB JSON_TEST ${CMAKE_CURRENT_SOURCE_DIR}/test/benchmark*.json)
-
-set(JSON_TEST_CODE_LIST)
-set(JSON_BENCH_CODE_LIST)
-foreach(it ${JSON_TEST})
-    set(JSON_TEST_CODE_LIST ${JSON_TEST_CODE_LIST} ${it})
-endforeach()
-
-foreach(it ${JSON_BENCH})
-    set(JSON_BENCH_CODE_LIST ${JSON_BENCH_CODE_LIST} ${it})
-endforeach()
-
-enable_testing()
-
-add_executable(test_mempool test/test_mempool.c)
-target_link_libraries(test_mempool kjson)
-add_test(kjson_test_mempool test_mempool)
-
-add_executable(test_kjson test/test_kjson.c)
-target_link_libraries(test_kjson kjson)
-add_test(kjson_test_kjson test_kjson ${JSON_TEST_CODE_LIST}
-        ${JSON_BENCH_CODE_LIST})
-
-add_executable(test_stream test/test_stream.c)
-target_link_libraries(test_stream kjson)
-add_test(kjson_test_stream test_stream ${JSON_TEST_CODE_LIST}
-        ${JSON_BENCH_CODE_LIST})
-
-add_executable(test_stringbuilder test/test_string_builder.c)
-add_test(kjson_test_stringbuilder test_stringbuilder)
-
-add_executable(test_stack test/test_kstack.c)
-add_test(kjson_test_stack test_stack)
-
-add_executable(test_tostring test/test_tostring.c)
-target_link_libraries(test_tostring kjson)
-add_test(kjson_test_tostring test_tostring)
-
-file(GLOB JSON_FAIL_TEST ${CMAKE_CURRENT_SOURCE_DIR}/test/test_json/fail*.json)
-file(GLOB JSON_PASS_TEST ${CMAKE_CURRENT_SOURCE_DIR}/test/test_json/pass*.json)
-add_executable(test_file test/test_file.c)
-
-foreach(f ${JSON_FAIL_TEST})
-	add_test(${f} test_file ${f} "F")
-endforeach()
-
-foreach(f ${JSON_PASS_TEST})
-	add_test(${f} test_file ${f} "T")
-endforeach()
-
-add_executable(test_kexception test/test_kexception.c)
-add_test(kjson_test_kexception test_kexception)
-
-#add_executable(test_float test/test_float.c)
-#add_test(kjson_test_float test_float)
-
-## Benchmark
-find_library(HAVE_LIBJSON json)
-find_library(HAVE_LIBJANSSON jansson)
-find_library(HAVE_LIBMSGPACK msgpack)
-find_library(HAVE_LIBYAJL yajl)
-set(bench1_libs)
-set(bench2_libs)
-
-if(HAVE_LIBMSGPACK)
-    add_definitions(-DUSE_MSGPACK)
-    set(bench1_libs ${bench1_libs} ${HAVE_LIBMSGPACK})
-endif()
-if(HAVE_LIBYAJL)
-    add_definitions(-DUSE_YAJL)
-    set(bench1_libs ${bench1_libs} ${HAVE_LIBYAJL})
-    set(bench2_libs ${bench2_libs} ${HAVE_LIBYAJL})
-    endif()
-if(HAVE_LIBJANSSON)
-    add_definitions(-DUSE_JANSSON)
-    set(bench2_libs ${bench2_libs} ${HAVE_LIBJANSSON})
-    elseif(HAVE_LIBJSON)
-    add_definitions(-DUSE_JSON_C)
-    set(bench2_libs ${bench2_libs} ${HAVE_LIBJSON})
-endif()
-
-add_executable(benchmark1 test/benchmark1.c)
-target_link_libraries(benchmark1 kjson ${bench1_libs})
-
-add_executable(benchmark2 test/benchmark2.cpp)
-target_link_libraries(benchmark2 kjson ${bench2_libs})
-
-add_executable(benchmark3 test/benchmark3.c)
-target_link_libraries(benchmark3 kjson)
-
-add_executable(benchmark4 test/benchmark4.c)
-target_link_libraries(benchmark4 kjson)
-
-add_executable(benchmark5 test/benchmark5.c)
-
-add_executable(parseString test/parseString.c)
-add_test(kjson_parseString parseString)
-target_link_libraries(parseString kjson)
-
-add_executable(check_cpp test/check_cpp_build.cpp)
-add_test(kjson_check_cpp check_cpp)
-
-add_executable(test_whitespace test/test_whitespace.c)
-add_test(kjson_test_whitespace test_whitespace)
-target_link_libraries(test_whitespace kjson)
-
-install(TARGETS kjson LIBRARY DESTINATION lib)
-
-file(GLOB headers ${CMAKE_CURRENT_SOURCE_DIR}/*.h)
-#message(STATUS ${headers})
-foreach(it ${headers})
-	install(FILES ${it} DESTINATION include/kjson)
-endforeach(it ${headers})
-
-MESSAGE(STATUS)
-MESSAGE(STATUS "---------------------------------------------------------------------------" )
-MESSAGE(STATUS "CMAKE_BUILD_TYPE     = ${uppercase_CMAKE_BUILD_TYPE}")
-MESSAGE(STATUS "CMAKE_C_COMPILER     = ${CMAKE_C_COMPILER}")
-MESSAGE(STATUS "CMAKE_CXX_COMPILER   = ${CMAKE_CXX_COMPILER}")
-MESSAGE(STATUS "CMAKE_C_FLAGS        = ${CMAKE_C_FLAGS_${uppercase_CMAKE_BUILD_TYPE}}")
-MESSAGE(STATUS "CMAKE_CXX_FLAGS      = ${CMAKE_CXX_FLAGS_${uppercase_CMAKE_BUILD_TYPE}}")
-MESSAGE(STATUS "CMAKE_INSTALL_PREFIX = ${CMAKE_INSTALL_PREFIX}")
-MESSAGE(STATUS "Change a value with: cmake -D<Variable>=<Value>" )
-MESSAGE(STATUS "---------------------------------------------------------------------------" )
-MESSAGE(STATUS)

src/module/Json/kjson/LICENSE
@@ -1,21 +1,0 @@
-Copyright (c) 2012, Masahiro Ide <ide@konohascript.org>
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
- * Redistributions of source code must retain the above copyright notice,
-   this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright
-   notice, this list of conditions and the following disclaimer in the
-   documentation and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
-CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.

src/module/Json/kjson/kexception.h
@@ -55,6 +55,7 @@ typedef struct kexception_handler {
 static inline void kexception_handler_reset(kexception_handler_t *eh)
 {
     memset(eh->handler, 0, sizeof(jmp_buf));
+    eh->has_error = 0;
 }
 
 static inline void kexception_handler_init(kexception_handler_t *eh)

src/module/Json/kjson/kjson.c
@@ -29,6 +29,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <inttypes.h>
+#include <limits.h>
 
 #ifdef __SSE2__
 #include <emmintrin.h>
@@ -46,9 +47,26 @@ extern "C" {
     }\
 } while(0)
 
-static inline JSON JSONError_new(const char *emessage)
+static inline bool JSON_CanFree(JSON json)
 {
-    return toJSON(ValueE(emessage));
+    JSONRC *rc = JSON_Reference(json);
+    return rc->count <= 0;
+}
+
+static inline void JSON_dispose(JSON json)
+{
+    JSONRC *rc = JSON_Reference(json);
+    rc->count = INT_MAX;
+}
+
+static inline JSON JSONError_new(JSONMemoryPool *jm, const char *emessage)
+{
+    bool malloced;
+    JSONError *o = (JSONError *) JSONMemoryPool_Alloc(jm, sizeof(*o), &malloced);
+    JSON json = toJSON(ValueE(o));
+    JSON_Init(json);
+    o->message = emessage;
+    return json;
 }
 
 static JSON JSONUString_new(JSONMemoryPool *jm, string_builder *builder)
@@ -94,13 +112,24 @@ KJSON_API void JSON_free(JSON o)
 static void JSONObject_free(JSON json)
 {
     JSONObject *o = toObj(json.val);
-    kmap_dispose(&o->child);
+    JSON_Release(json);
+    if (JSON_CanFree(json)) {
+        kmap_dispose(&o->child);
+        JSON_dispose(json);
+        JSONMemoryPool_Free(0, o);
+    }
 }
 
 static void _JSONString_free(JSONString *obj)
 {
-    if(obj->length > JSONSTRING_INLINE_SIZE) {
-        free((char *)obj->str);
+    JSON json = toJSON(ValueS(obj));
+    JSON_Release(json);
+    if (JSON_CanFree(json)) {
+        JSON_dispose(json);
+        if(obj->length > JSONSTRING_INLINE_SIZE) {
+            free((char *)obj->str);
+        }
+        JSONMemoryPool_Free(0, obj);
     }
 }
 
@@ -113,13 +142,17 @@ static void JSONString_free(JSON json)
 static void JSONArray_free(JSON json)
 {
     JSONArray *a = toAry(json.val);
-    JSON *s, *e;
+    JSON_Release(json);
+    if (JSON_CanFree(json)) {
+        JSON *s, *e;
 
-    FOR_EACH_ARRAY(a->array, s, e) {
-        _JSON_free(*s);
+        FOR_EACH_ARRAY(a->array, s, e) {
+            _JSON_free(*s);
+        }
+        JSON_dispose(json);
+        ARRAY_dispose(JSON, &a->array);
+        JSONMemoryPool_Free(0, a);
     }
-
-    ARRAY_dispose(JSON, &a->array);
 }
 
 #define JSON_OP(OP)\
@@ -173,6 +206,9 @@ KJSON_API void JSONArray_append(JSONMemoryPool *jm, JSON json, JSON o)
 static void _JSONObject_set(JSONObject *o, JSONString *key, JSON value)
 {
     assert(JSON_type(value) < 16);
+    if ((JSON_type(value) & 1) == 1) {
+        JSON_Retain(value);
+    }
     kmap_set(&o->child, key, value.bits);
 }
 
@@ -653,7 +689,7 @@ KJSON_API JSON parseJSON(JSONMemoryPool *jm, const char *s, const char *e)
     }
     CATCH(PARSER_EXCEPTION) {
         const char *emessage = ins->exception.error_message;
-        json = JSONError_new(emessage);
+        json = JSONError_new(jm, emessage);
         ins->exception.has_error = 1;
         goto L_finally;
     }
@@ -672,6 +708,8 @@ KJSON_API JSON JSON_get(JSON json, const char *key, size_t len)
     tmp.length = len;
     tmp.hashcode = 0;
     map_record_t *r = kmap_get(&o->child, &tmp);
+    if (r) {
+    }
     return (r) ? toJSON(ValueP(r->v)) : JSON_NOP();
 }
 

src/module/Json/kjson/kjson.h
@@ -65,8 +65,13 @@ typedef enum kjson_type {
 union JSONValue;
 typedef union JSONValue JSON;
 
+typedef struct JSONRC {
+    long count;
+} JSONRC;
+
 #define JSONSTRING_INLINE_SIZE (sizeof(void *)*2)
 typedef struct JSONString {
+    JSONRC rc;
     const char *str;
     unsigned length;
     unsigned hashcode;
@@ -78,6 +83,7 @@ DEF_ARRAY_STRUCT0(JSON, unsigned);
 DEF_ARRAY_T(JSON);
 
 typedef struct JSONArray {
+    JSONRC rc;
     ARRAY(JSON) array;
 } JSONArray;
 
@@ -89,19 +95,28 @@ typedef JSONNumber JSONDouble;
 typedef JSONNumber JSONBool;
 
 typedef struct JSONInt64 {
+    JSONRC rc;
     int64_t val;
 } JSONInt64;
 
 typedef struct JSONObject {
+    JSONRC rc;
     kmap_t child;
 } JSONObject;
+
+typedef struct JSONError {
+    JSONRC rc;
+    const char *message;
+} JSONError;
 
 union JSONValue {
     Value       val;
     JSONNumber  num;
+    JSONInt64  *box;
     JSONString *str;
     JSONArray  *ary;
     JSONObject *obj;
+    JSONError  *err;
     uint64_t bits;
 };
 
@@ -124,6 +139,45 @@ static inline JSON JSON_NOP(void)
 static inline bool JSON_isValid(JSON json)
 {
     return json.bits != 0;
+}
+
+static inline kjson_type JSON_type(JSON json) {
+    Value v; v.bits = (uint64_t)json.val.bits;
+    uint64_t tag = Tag(v);
+    return (IsDouble((v)))?
+        JSON_Double : (kjson_type) ((tag >> TagBitShift) & 15);
+}
+
+/* JSON Reference Count API */
+static inline JSONRC *JSON_Reference(JSON json)
+{
+    JSONObject *o = toObj(json.val);
+    return &o->rc;
+}
+
+static inline void JSON_Init(JSON json)
+{
+    JSONRC *rc = JSON_Reference(json);
+    rc->count = 0;
+}
+
+static inline void JSON_Retain(JSON json)
+{
+    JSONRC *rc = JSON_Reference(json);
+    rc->count += 1;
+}
+
+static inline void JSONObject_Retain(JSON json)
+{
+    if ((JSON_type(json) & 1) == 1) {
+        JSON_Retain(json);
+    }
+}
+
+static inline void JSON_Release(JSON json)
+{
+    JSONRC *rc = JSON_Reference(json);
+    rc->count -= 1;
 }
 
 /* [Getter API] */
@@ -178,13 +232,6 @@ static inline char *JSON_toString(JSON json)
     size_t len;
     char *s = JSON_toStringWithLength(json, &len);
     return s;
-}
-
-static inline kjson_type JSON_type(JSON json) {
-    Value v; v.bits = (uint64_t)json.val.bits;
-    uint64_t tag = Tag(v);
-    return (IsDouble((v)))?
-        JSON_Double : (kjson_type) ((tag >> TagBitShift) & 15);
 }
 
 #define JSON_TYPE_CHECK(T, O) (JSON_type(((JSON)O)) == JSON_##T)
@@ -285,10 +332,12 @@ static inline JSON JSONString_new(JSONMemoryPool *jm, const char *s, size_t len)
 {
     bool malloced;
     JSONString *o = (JSONString *) JSONMemoryPool_Alloc(jm, sizeof(*o), &malloced);
+    JSON json = toJSON(ValueS(o));
+    JSON_Init(json);
     char *str = (len > JSONSTRING_INLINE_SIZE) ? (char *) malloc(len) : o->text;
     memcpy(str, s, len);
     JSONString_init(o, (const char *)str, len);
-    return toJSON(ValueS(o));
+    return json;
 }
 
 static inline JSON JSONNull_new()
@@ -300,16 +349,20 @@ static inline JSON JSONObject_new(JSONMemoryPool *jm, unsigned map_size)
 {
     bool malloced;
     JSONObject *o = (JSONObject *) JSONMemoryPool_Alloc(jm, sizeof(*o), &malloced);
+    JSON json = toJSON(ValueO(o));
+    JSON_Init(json);
     kmap_init(&(o->child), map_size);
-    return toJSON(ValueO(o));
+    return json;
 }
 
 static inline JSON JSONArray_new(JSONMemoryPool *jm, unsigned elm_size)
 {
     bool malloced;
     JSONArray *o = (JSONArray *) JSONMemoryPool_Alloc(jm, sizeof(*o), &malloced);
+    JSON json = toJSON(ValueA(o));
+    JSON_Init(json);
     ARRAY_init(JSON, &o->array, elm_size);
-    return toJSON(ValueA(o));
+    return json;
 }
 
 static inline JSON JSONDouble_new(double val)
@@ -344,7 +397,7 @@ static inline unsigned JSON_length(JSON json)
 
 static inline const char *JSONError_get(JSON json)
 {
-    return toError(json.val);
+    return toError(json.val)->message;
 }
 
 static inline JSON JSONArray_get(JSON json, unsigned index)

commit 83f40e3f80664ca977ef331b4478bfaf3ed6ee1e

bug fixed AsJSON macro caused by 72d1f90944

src/module/Json/Json.c
@@ -35,7 +35,8 @@ extern "C" {
 struct JsonBuf {
 	uint64_t json_i;
 };
-#define AsJSON(JSONBUF) (toJSON(jsonbuf->json_i))
+
+#define AsJSON(JSONBUF) (toJSON(JSONBUF->json_i))
 
 static kbool_t IsJsonType(struct JsonBuf *jsonbuf, KJSONTYPE type)
 {