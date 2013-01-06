24a6cb4283e3d49089f443b565993843109fe3fb
Toshiaki NOZAWA <t_nozawa@mebius.co.jp>
2013-01-07 02:01:24 +0900

    add "System.event_add()" method, and success to compile.

--- src/package-devel/libevent/libevent_glue.c
+++ src/package-devel/libevent/libevent_glue.c
@@ -35,7 +35,7 @@ extern "C" {
 static void Libevent_Init(KonohaContext *kctx, kObject *o, void *conf)
 {
 	struct Libevent *ev = (struct Libevent *) o;
-	ev->event_base = NULL/*?*/;	//refered jansson_glue.c
+	ev->event_base = NULL;
 }
 
 static void Libevent_Free(KonohaContext *kctx, kObject *o)
@@ -48,43 +48,33 @@ static void Libevent_Free(KonohaContext *kctx, kObject *o)
 	}
 }
 
-static void Libevent_p(KonohaContext *kctx, KonohaValue *v, int pos, KBuffer *wb)
-{
-#if 0	
-	/*
-	Is event_base able to serialize?
-	e.g. including FileDescriptor, kqueue...
-	*/
-
-	/* This function is called when serializing the object. */
-	struct Person *p = (struct Person *) v[pos].asObject;
-	KLIB KBuffer_Write(kctx, wb, kString_text(p->name), kString_size(p->name));
-	KLIB KBuffer_Write(kctx, wb, ",", 1);
-	KLIB KBuffer_printf(kctx, wb, KINT_FMT, p->age);
-#endif
-}
-
-static void Libevent_Reftrace(KonohaContext *kctx, kObject *o, KObjectVisitor *visitor)
-{
-#if 0
-	/* Garbage collector (GC) cannot recognize in which position of the field
-	 * an object exists. The function tells to GC which object should be traced. */
-	struct Person *p = (struct Person *) o;
-	/* If p->some_field is Nullable, please use 
-	 * KRefTraceNullable() macro instead of KRefTrace(). */
-	KRefTrace(p->name);
-	/* It is not necessary to trace p->age field,
-	 * because p->age is not an Object */
-#endif
-}
-
 //## Libevent Libevent.new();
 static KMETHOD Libevent_new(KonohaContext *kctx, KonohaStack *sfp)
 {
-	struct Libevent *ev = (struct Libevent *) sfp[0].asObject;
+	kLibevent *ev = (kLibevent *)sfp[0].asObject;
 	ev->event_base = event_base_new();
 	KReturn(ev);
 }
+
+/* ======================================================================== */
+//## int System.event_add(Libevent_event event, Date tv);
+KMETHOD System_event_add(KonohaContext *kctx, KonohaStack* sfp)
+{
+#define Declare_kDate
+#ifdef Declare_kDate	//TODO:It should be declared in headerfile in js4.date package.
+	typedef struct kDateVar {
+		kObjectHeader h;
+		struct timeval tv;
+	} kDate;
+#endif	//Declare_kDate
+#undef Declare_kDate
+
+	kLibevent_event *ev = (kLibevent_event *)sfp[1].asObject;
+	kDate *date = (kDate *)sfp[2].asObject;
+	int ret = event_add(ev->event, &date->tv);
+	KReturnUnboxValue(ret);
+}
+
 static kbool_t Libevent_PackupNameSpace(KonohaContext *kctx, kNameSpace *ns, int option, KTraceInfo *trace)
 {
 	/* Class Definition */
@@ -93,8 +83,6 @@ static kbool_t Libevent_PackupNameSpace(KonohaContext *kctx, kNameSpace *ns, int
 	SETSTRUCTNAME(defLibevent, Libevent);
 	defLibevent.cflag     = KClassFlag_Final;
 	defLibevent.init      = Libevent_Init;
-	defLibevent.p         = Libevent_p;
-	defLibevent.reftrace  = Libevent_Reftrace;
 	defLibevent.free      = Libevent_Free;
 	KClass *LibeventClass = KLIB kNameSpace_DefineClass(kctx, ns, NULL, &defLibevent, trace);
 
@@ -103,6 +91,10 @@ static kbool_t Libevent_PackupNameSpace(KonohaContext *kctx, kNameSpace *ns, int
 
 	KDEFINE_METHOD MethodData[] = {
 		_Public, _F(Libevent_new), KType_Libevent, KType_Libevent, KKMethodName_("new"), 0,
+		_Public|_Static|_Const|_Im, _F(System_event_add), KType_int, KType_System, KKMethodName_("event_add"), 2, KType_Object, KFieldName_("Libevent_event"), KType_Object, KFieldName_("timeval"),	//TODO: param type should be "KType_Libevent_event" "KType_Date"
+
+
+
 #ifdef	CUTCUT
 		_Public, _F(Person_say), KType_String, KType_Person, KKMethodName_("say"), 0,
 #endif
