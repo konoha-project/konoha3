51f7d20c29bc3e2657820b3262360eeda49a1484
Toshiaki NOZAWA <t_nozawa@mebius.co.jp>
2013-01-06 18:15:20 +0900

    add libevent_event_glue.[ck], and successt to compile

--- src/package-devel/libevent/libevent_event_glue.c
+++ src/package-devel/libevent/libevent_event_glue.c
@@ -32,80 +32,50 @@
 extern "C" {
 #endif
 
-static void Libevent_Init(KonohaContext *kctx, kObject *o, void *conf)
+static void Libevent_event_Init(KonohaContext *kctx, kObject *o, void *conf)
 {
-	struct Libevent *ev = (struct Libevent *) o;
-	ev->event_base = NULL/*?*/;	//refered jansson_glue.c
+	struct Libevent_event *ev = (struct Libevent_event *) o;
+	ev->event = NULL;
 }
 
-static void Libevent_Free(KonohaContext *kctx, kObject *o)
+static void Libevent_event_Free(KonohaContext *kctx, kObject *o)
 {
-	struct Libevent *ev = (struct Libevent *) o;
+	struct Libevent_event *ev = (struct Libevent_event *) o;
 
-	if (ev->event_base != NULL) {
-		event_base_free(ev->event_base);
-		ev->event_base = NULL;
+	if (ev->event != NULL) {
+		event_free(ev->event);
+		ev->event = NULL;
 	}
 }
 
-static void Libevent_p(KonohaContext *kctx, KonohaValue *v, int pos, KBuffer *wb)
+//## Libevent_event Libevent_event.new();
+static KMETHOD Libevent_event_new(KonohaContext *kctx, KonohaStack *sfp)
 {
-#if 0	
-	/*
-	Is event_base able to serialize?
-	e.g. including FileDescriptor, kqueue...
-	*/
-
-	/* This function is called when serializing the object. */
-	struct Person *p = (struct Person *) v[pos].asObject;
-	KLIB KBuffer_Write(kctx, wb, kString_text(p->name), kString_size(p->name));
-	KLIB KBuffer_Write(kctx, wb, ",", 1);
-	KLIB KBuffer_printf(kctx, wb, KINT_FMT, p->age);
-#endif
-}
-
-static void Libevent_Reftrace(KonohaContext *kctx, kObject *o, KObjectVisitor *visitor)
-{
-#if 0
-	/* Garbage collector (GC) cannot recognize in which position of the field
-	 * an object exists. The function tells to GC which object should be traced. */
-	struct Person *p = (struct Person *) o;
-	/* If p->some_field is Nullable, please use 
-	 * KRefTraceNullable() macro instead of KRefTrace(). */
-	KRefTrace(p->name);
-	/* It is not necessary to trace p->age field,
-	 * because p->age is not an Object */
-#endif
-}
-
-//## Libevent Libevent.new();
-static KMETHOD Libevent_new(KonohaContext *kctx, KonohaStack *sfp)
-{
-	struct Libevent *ev = (struct Libevent *) sfp[0].asObject;
-	ev->event_base = event_base_new();
+	struct Libevent_event *ev = (struct Libevent_event *) sfp[0].asObject;
+	struct Libevent *libevent = (struct Libevent *)sfp[1].asObject;
+	evutil_socket_t fd = (evutil_socket_t)sfp[2].intValue;
+	kint_t what = sfp[3].intValue;
+	event_callback_fn cb = (event_callback_fn)sfp[4].asFunc;
+	void *arg = (void *)sfp[5].asObject;
+	ev->event = event_new(libevent->event_base, fd, (short)what, cb, arg);
 	KReturn(ev);
 }
-static kbool_t Libevent_PackupNameSpace(KonohaContext *kctx, kNameSpace *ns, int option, KTraceInfo *trace)
+static kbool_t Libevent_event_PackupNameSpace(KonohaContext *kctx, kNameSpace *ns, int option, KTraceInfo *trace)
 {
 	/* Class Definition */
 	/* If you want to create Generic class like Array<T>, see konoha.map package */
-	KDEFINE_CLASS defLibevent = {0};
-	SETSTRUCTNAME(defLibevent, Libevent);
-	defLibevent.cflag     = KClassFlag_Final;
-	defLibevent.init      = Libevent_Init;
-	defLibevent.p         = Libevent_p;
-	defLibevent.reftrace  = Libevent_Reftrace;
-	defLibevent.free      = Libevent_Free;
-	KClass *LibeventClass = KLIB kNameSpace_DefineClass(kctx, ns, NULL, &defLibevent, trace);
+	KDEFINE_CLASS defLibevent_event = {0};
+	SETSTRUCTNAME(defLibevent_event, Libevent_event);
+	defLibevent_event.cflag     = KClassFlag_Final;
+	defLibevent_event.init      = Libevent_event_Init;
+	defLibevent_event.free      = Libevent_event_Free;
+	KClass *Libevent_eventClass = KLIB kNameSpace_DefineClass(kctx, ns, NULL, &defLibevent_event, trace);
 
 	/* You can define methods with the following procedures. */
-	int KType_Libevent = LibeventClass->typeId;
+	int KType_Libevent_event = Libevent_eventClass->typeId;
 
 	KDEFINE_METHOD MethodData[] = {
-		_Public, _F(Libevent_new), KType_Libevent, KType_Libevent, KKMethodName_("new"), 0,
-#ifdef	CUTCUT
-		_Public, _F(Person_say), KType_String, KType_Person, KKMethodName_("say"), 0,
-#endif
+		_Public, _F(Libevent_event_new), KType_Libevent_event, KType_Libevent_event, KKMethodName_("new"), 0,
 		DEND, /* <= sentinel */
 	};
 	KLIB kNameSpace_LoadMethodData(kctx, ns, MethodData, trace);
@@ -121,17 +91,17 @@ static kbool_t Libevent_PackupNameSpace(KonohaContext *kctx, kNameSpace *ns, int
 	return true;
 }
 
-static kbool_t Libevent_ExportNameSpace(KonohaContext *kctx, kNameSpace *ns, kNameSpace *exportNS, int option, KTraceInfo *trace)
+static kbool_t Libevent_event_ExportNameSpace(KonohaContext *kctx, kNameSpace *ns, kNameSpace *exportNS, int option, KTraceInfo *trace)
 {
 	return true;
 }
 
-KDEFINE_PACKAGE *libevent_Init(void)
+KDEFINE_PACKAGE *libevent_event_Init(void)
 {
 	static KDEFINE_PACKAGE d = {0};
 	KSetPackageName(d, "libevent2", "0.1");
-	d.PackupNameSpace    = Libevent_PackupNameSpace;
-	d.ExportNameSpace   = Libevent_ExportNameSpace;
+	d.PackupNameSpace    = Libevent_event_PackupNameSpace;
+	d.ExportNameSpace   = Libevent_event_ExportNameSpace;
 	return &d;
 }
 
