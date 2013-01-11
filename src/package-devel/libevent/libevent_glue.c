9d3d91bcd0fcac972e988ed9fadc3f5b398c3be7
Toshiaki NOZAWA <t_nozawa@mebius.co.jp>
2013-01-11 17:43:39 +0900

    rename Libevent_event => Event

--- src/package-devel/libevent/libevent_glue.c
+++ src/package-devel/libevent/libevent_glue.c
@@ -31,15 +31,12 @@
 extern "C" {
 #endif
 
+/* ======================================================================== */
+// Event_base class
 typedef struct Event_base {
 	kObjectHeader h;
 	struct event_base *event_base;
 } kEvent_base;
-
-typedef struct Libevent_event {
-	kObjectHeader h;
-	struct event *event;
-} kLibevent_event;
 
 static void Event_base_Init(KonohaContext *kctx, kObject *o, void *conf)
 {
@@ -73,7 +70,46 @@ static KMETHOD Event_base_dispatch(KonohaContext *kctx, KonohaStack *sfp)
 	KReturnUnboxValue(ret);
 }
 
+
 /* ======================================================================== */
+// Event class
+typedef struct Event {
+	kObjectHeader h;
+	struct event *event;
+} kEvent;
+
+static void Event_Init(KonohaContext *kctx, kObject *o, void *conf)
+{
+	struct Event *ev = (struct Event *) o;
+	ev->event = NULL;
+}
+
+static void Event_Free(KonohaContext *kctx, kObject *o)
+{
+	struct Event *ev = (struct Event *) o;
+
+	if (ev->event != NULL) {
+		event_free(ev->event);
+		ev->event = NULL;
+	}
+}
+
+//## Event Event.new();
+static KMETHOD Event_new(KonohaContext *kctx, KonohaStack *sfp)
+{
+	struct Event *ev = (struct Event *) sfp[0].asObject;
+	struct Event_base *libevent = (struct Event_base *)sfp[1].asObject;
+	evutil_socket_t fd = (evutil_socket_t)sfp[2].intValue;
+	kint_t what = sfp[3].intValue;
+	event_callback_fn cb = (event_callback_fn)sfp[4].asFunc;
+	void *arg = (void *)sfp[5].asObject;
+	ev->event = event_new(libevent->event_base, fd, (short)what, cb, arg);
+	KReturn(ev);
+}
+
+
+/* ======================================================================== */
+// System class
 //## int System.event_add(Event_base_event event, Date tv);
 KMETHOD System_event_add(KonohaContext *kctx, KonohaStack* sfp)
 {
@@ -86,48 +122,22 @@ KMETHOD System_event_add(KonohaContext *kctx, KonohaStack* sfp)
 #endif	//Declare_kDate
 #undef Declare_kDate
 
-	kLibevent_event *ev = (kLibevent_event *)sfp[1].asObject;
+	kEvent *ev = (kEvent *)sfp[1].asObject;
 	kDate *date = (kDate *)sfp[2].asObject;
 	int ret = event_add(ev->event, &date->tv);
 	KReturnUnboxValue(ret);
 }
 
-//## int System.event_del(Libevent_event event);
+//## int System.event_del(Event event);
 KMETHOD System_event_del(KonohaContext *kctx, KonohaStack* sfp)
 {
-	kLibevent_event *ev = (kLibevent_event *)sfp[1].asObject;
+	kEvent *ev = (kEvent *)sfp[1].asObject;
 	int ret = event_del(ev->event);
 	KReturnUnboxValue(ret);
 }
 
-static void Libevent_event_Init(KonohaContext *kctx, kObject *o, void *conf)
-{
-	struct Libevent_event *ev = (struct Libevent_event *) o;
-	ev->event = NULL;
-}
 
-static void Libevent_event_Free(KonohaContext *kctx, kObject *o)
-{
-	struct Libevent_event *ev = (struct Libevent_event *) o;
-
-	if (ev->event != NULL) {
-		event_free(ev->event);
-		ev->event = NULL;
-	}
-}
-
-//## Libevent_event Libevent_event.new();
-static KMETHOD Libevent_event_new(KonohaContext *kctx, KonohaStack *sfp)
-{
-	struct Libevent_event *ev = (struct Libevent_event *) sfp[0].asObject;
-	struct Event_base *libevent = (struct Event_base *)sfp[1].asObject;
-	evutil_socket_t fd = (evutil_socket_t)sfp[2].intValue;
-	kint_t what = sfp[3].intValue;
-	event_callback_fn cb = (event_callback_fn)sfp[4].asFunc;
-	void *arg = (void *)sfp[5].asObject;
-	ev->event = event_new(libevent->event_base, fd, (short)what, cb, arg);
-	KReturn(ev);
-}
+/* ======================================================================== */
 
 static kbool_t Event_base_PackupNameSpace(KonohaContext *kctx, kNameSpace *ns, int option, KTraceInfo *trace)
 {
@@ -140,25 +150,25 @@ static kbool_t Event_base_PackupNameSpace(KonohaContext *kctx, kNameSpace *ns, i
 	defEvent_base.free      = Event_base_Free;
 	KClass *Event_baseClass = KLIB kNameSpace_DefineClass(kctx, ns, NULL, &defEvent_base, trace);
 
-	KDEFINE_CLASS defLibevent_event = {0};
-	SETSTRUCTNAME(defLibevent_event, Libevent_event);
-	defLibevent_event.cflag     = KClassFlag_Final;
-	defLibevent_event.init      = Libevent_event_Init;
-	defLibevent_event.free      = Libevent_event_Free;
-	KClass *Libevent_eventClass = KLIB kNameSpace_DefineClass(kctx, ns, NULL, &defLibevent_event, trace);
+	KDEFINE_CLASS defEvent = {0};
+	SETSTRUCTNAME(defEvent, Event);
+	defEvent.cflag     = KClassFlag_Final;
+	defEvent.init      = Event_Init;
+	defEvent.free      = Event_Free;
+	KClass *EventClass = KLIB kNameSpace_DefineClass(kctx, ns, NULL, &defEvent, trace);
 
 
 	/* You can define methods with the following procedures. */
 	int KType_Event_base = Event_baseClass->typeId;
-	int KType_Libevent_event = Libevent_eventClass->typeId;
+	int KType_Event = EventClass->typeId;
 
 	KDEFINE_METHOD MethodData[] = {
 		_Public, _F(Event_base_new), KType_Event_base, KType_Event_base, KMethodName_("new"), 0,
 		_Public, _F(Event_base_dispatch), KType_Event_base, KType_Event_base, KMethodName_("dispatch"), 0,
-		_Public|_Static|_Const|_Im, _F(System_event_add), KType_int, KType_System, KMethodName_("event_add"), 2, KType_Object, KFieldName_("Libevent_event"), KType_Object, KFieldName_("timeval"),	//TODO: param type should be "KType_Libevent_event" "KType_Date"
-		_Public|_Static|_Const|_Im, _F(System_event_del), KType_int, KType_System, KMethodName_("event_del"), 1, KType_Object, KFieldName_("Libevent_event"),	//TODO: param type should be "KType_Libevent_event"
+		_Public|_Static|_Const|_Im, _F(System_event_add), KType_int, KType_System, KMethodName_("event_add"), 2, KType_Object, KFieldName_("Event"), KType_Object, KFieldName_("timeval"),	//TODO: param type should be "KType_Event" "KType_Date"
+		_Public|_Static|_Const|_Im, _F(System_event_del), KType_int, KType_System, KMethodName_("event_del"), 1, KType_Object, KFieldName_("Event"),	//TODO: param type should be "KType_Event"
 
-		_Public, _F(Libevent_event_new), KType_Libevent_event, KType_Libevent_event, KMethodName_("new"), 0,
+		_Public, _F(Event_new), KType_Event, KType_Event, KMethodName_("new"), 0,
 
 
 #ifdef	CUTCUT
