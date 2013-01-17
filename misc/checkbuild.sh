4d1cd886f678d3778ae3934ae217c40f05902b3b
Masahiro Ide <imasahiro9@gmail.com>
2013-01-18 00:23:23 +0900

    update checkbuild script

--- misc/checkbuild.sh
+++ misc/checkbuild.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-BRANCH=-`git branch --contains=HEAD | cut -c 3-`
+BRANCH=-`git branch --contains=HEAD | grep "*" | cut -c 3-`
 DEFAULT=Debug
 
 if [ "${BRANCH}" = "-" ]; then
@@ -12,10 +12,10 @@ do
   mkdir -p $i${BRANCH}
   cd $i${BRANCH}
   cmake ../ -DCMAKE_BUILD_TYPE=$i -DCMAKE_INSTALL_PREFIX=$HOME
-  make 1> /dev/null 2> $i-log || exit
+  make -j8 1> /dev/null 2> $i-log || exit
 )
 done
 
-make -C ${DEFAULT}${BRANCH} > /dev/null
-make -C ${DEFAULT}${BRANCH} install > /dev/null
+make -C ${DEFAULT}${BRANCH} -j8 > /dev/null
+make -C ${DEFAULT}${BRANCH} -j8 install > /dev/null
 make -C ${DEFAULT}${BRANCH} test
