9d8bf65159a98da5e81e3b2257c4451309a3c0dd
Masahiro Ide <imasahiro9@gmail.com>
2013-02-19 23:57:38 +0900

    fix libevent path

--- cmake/FindLibEvent.cmake
+++ cmake/FindLibEvent.cmake
@@ -12,9 +12,9 @@ endif(EVENT_INCLUDE_DIR)
 
 if(APPLE)
 	# support MacPorts
-	FIND_PATH(EVENT_INCLUDE_DIR event.h
+	FIND_PATH(EVENT_INCLUDE_DIR event2/event.h
 		PATHS
-		/opt/local/include/event2
+		/opt/local/include/
 		NO_CMAKE_SYSTEM_PATH
 	)
 	FIND_LIBRARY(EVENT_LIBRARIES NAMES event libevent
@@ -24,7 +24,7 @@ if(APPLE)
 	)
 endif(APPLE)
 
-FIND_PATH(EVENT_INCLUDE_DIR event.h PATHS /opt/local/include/event2)
+FIND_PATH(EVENT_INCLUDE_DIR event2/event.h PATHS /opt/local/include/)
 string(REGEX REPLACE "(.*)/include/?" "\\1" EVENT_INCLUDE_BASE_DIR "${EVENT_INCLUDE_DIR}")
 
 FIND_LIBRARY(EVENT_LIBRARIES NAMES event libevent
