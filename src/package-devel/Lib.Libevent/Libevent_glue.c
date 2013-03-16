77f82f023c003d2f1d55408b195387a64ca4f109
Toshiaki NOZAWA <t_nozawa@mebius.co.jp>
2013-03-16 17:30:30 +0900

    add libevent version comparison for support APIs

--- src/package-devel/Lib.Libevent/Libevent_glue.c
+++ src/package-devel/Lib.Libevent/Libevent_glue.c
@@ -1198,7 +1198,7 @@ static KMETHOD cevhttp_connection_new(KonohaContext *kctx, KonohaStack *sfp)
 	KReturn(con);
 }
 
-#ifdef LIBEVENT_2_0_17_LATER
+#if (_EVENT_NUMERIC_VERSION >= 0x02001100)	//LIBEVENT_2_0_17_LATER
 //## bufferevent evhttp_connection.get_bufferevent();
 static KMETHOD cevhttp_connection_get_bufferevent(KonohaContext *kctx, KonohaStack *sfp)
 {
@@ -1419,7 +1419,7 @@ static KMETHOD cevhttp_uri_new(KonohaContext *kctx, KonohaStack *sfp)
 	KReturn(uri);
 }
 
-#ifdef LIBEVENT_2_0_11_LATER
+#if (_EVENT_NUMERIC_VERSION >= 0x02000b00)	//LIBEVENT_2_0_11_LATER
 //## void evhttp_uri.set_flags(int flags);
 static KMETHOD cevhttp_uri_set_flags(KonohaContext *kctx, KonohaStack *sfp)
 {
@@ -1541,7 +1541,7 @@ static KMETHOD cevhttp_uri_set_fragment(KonohaContext *kctx, KonohaStack *sfp)
 	KReturnUnboxValue(evhttp_uri_set_fragment(uri->uri, fragment->text));
 }
 
-#if LIBEVENT_2_0_11_LATER
+#if (_EVENT_NUMERIC_VERSION >= 0x02000b00)	//LIBEVENT_2_0_11_LATER
 //## evhttp_uri evhttp_uri.parse_with_flags(String source_uri, int flags);
 static KMETHOD cevhttp_uri_parse_with_flags(KonohaContext *kctx, KonohaStack *sfp)
 {
@@ -2021,7 +2021,7 @@ static kbool_t Libevent_PackupNameSpace(KonohaContext *kctx, kNameSpace *ns, int
 
 		// cevhttp_connection
 		_Public, _F(cevhttp_connection_new), KType_cevhttp_connection, KType_cevhttp_connection, KMethodName_("new"), 4, KType_cevent_base, KFieldName_("event_base"), KType_cevdns_base, KFieldName_("dnsbase"), KType_String, KFieldName_("address"), KType_Int, KFieldName_("port"),
-#ifdef LIBEVENT_2_0_17_LATER
+#if (_EVENT_NUMERIC_VERSION >= 0x02001100)	//LIBEVENT_2_0_17_LATER
 		_Public, _F(cevhttp_connection_get_bufferevent), KType_cevbuffer, KType_cevhttp_connection, KMethodName_("get_bufferevent"), 0,
 #endif
 		_Public, _F(cevhttp_connection_get_base), KType_cevent_base, KType_cevhttp_connection, KMethodName_("get_base"), 0,
@@ -2043,7 +2043,7 @@ static kbool_t Libevent_PackupNameSpace(KonohaContext *kctx, kNameSpace *ns, int
 
 		// cevhttp_uri
 		_Public, _F(cevhttp_uri_new), KType_cevhttp_uri, KType_cevhttp_uri, KMethodName_("new"), 0,
-#if LIBEVENT_2_0_11_LATER
+#if (_EVENT_NUMERIC_VERSION >= 0x02000b00)	//LIBEVENT_2_0_11_LATER
 		_Public, _F(cevhttp_uri_set_flags), KType_void, KType_cevhttp_uri, KMethodName_("set_flags"), 1, KType_Int, KFieldName_("flags"),
 #endif
 		_Public|_Im, _F(cevhttp_uri_get_scheme), KType_String, KType_cevhttp_uri, KMethodName_("get_scheme"), 0,
@@ -2060,7 +2060,7 @@ static kbool_t Libevent_PackupNameSpace(KonohaContext *kctx, kNameSpace *ns, int
 		_Public, _F(cevhttp_uri_set_path), KType_Int, KType_cevhttp_uri, KMethodName_("set_path"), 1, KType_String, KFieldName_("path"),
 		_Public, _F(cevhttp_uri_set_query), KType_Int, KType_cevhttp_uri, KMethodName_("set_query"), 1, KType_String, KFieldName_("query"),
 		_Public, _F(cevhttp_uri_set_fragment), KType_Int, KType_cevhttp_uri, KMethodName_("set_fragment"), 1, KType_String, KFieldName_("fragment"),
-#if LIBEVENT_2_0_11_LATER
+#if (_EVENT_NUMERIC_VERSION >= 0x02000b00)	//LIBEVENT_2_0_11_LATER
 		_Public, _F(cevhttp_uri_parse_with_flags), KType_cevhttp_uri, KType_cevhttp_uri, KMethodName_("parse_with_flags"), 2, KType_String, KFieldName_("source_uri"), KType_Int, KFieldName_("flags"),
 #endif
 		_Public, _F(cevhttp_uri_parse), KType_cevhttp_uri, KType_cevhttp_uri, KMethodName_("parse"), 1, KType_String, KFieldName_("source_uri"),
