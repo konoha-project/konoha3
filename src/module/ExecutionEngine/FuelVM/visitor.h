c49b64f0acca7028f01b33aa6864677119e1d338
Masahiro Ide <imasahiro9@gmail.com>
2013-03-22 08:30:46 +0900

    update licence

--- src/module/ExecutionEngine/FuelVM/visitor.h
+++ src/module/ExecutionEngine/FuelVM/visitor.h
@@ -1,5 +1,5 @@
 /****************************************************************************
- * Copyright (c) 2012, Masahiro Ide <ide@konohascript.org> All rights reserved.
+ * Copyright (c) 2012-2013, Masahiro Ide <ide@konohascript.org> All rights reserved.
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
